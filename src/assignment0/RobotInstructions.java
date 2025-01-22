2437f6c9ad020eb941141c2920e3ac8035cfc678
emily <emily@gregory.dhcp.wustl.edu>
2025-01-22 12:11:53 -0600

    letter E

--- src/assignment0/RobotInstructions.java
+++ src/assignment0/RobotInstructions.java
@@ -29,11 +29,21 @@ public class RobotInstructions  {
 		 * You will add instructions at the end of the code to cause the support.robot to move
 		 * as you desire.
 		 */
-		robot.forward(60);
 		robot.turnLeft(90);
-		robot.forward(23);
+		robot.forward(200);
 		robot.turnRight(90);
-		robot.forward(80);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(180);
+		robot.turnRight(180);
+		robot.forward(180);
+		robot.turnRight(90);
+		robot.forward(100);
+		robot.turnRight(90);
+		robot.forward(200);
+		
+		
+		
 	}
 
 }
